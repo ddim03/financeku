4be5e7736b6ea5362f66eeec20d0cc5160eaaed8
dimasgilang <itsme.dimas03@gmail.com>
2024-09-13 19:43:02 +0700

    feat: membuat middleware untuk hak akses
    Membuat middleware RoleMiddleware.php yang menerima parameter string role untuk melakukan check apakah role user yang sedang login sesuai dengan parameter yang dikirim pada route yang dipasangi middleware

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('teller', TellerController::class);
-Route::put('/teller/{teller}/block', [TellerController::class, 'block'])->name('teller.block');
+Route::middleware(['auth', 'role:manager'])->group(function () {
+    Route::resource('teller', TellerController::class);
+    Route::put('/teller/{teller}/block', [TellerController::class, 'block'])
+        ->name('teller.block');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
